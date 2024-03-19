aaa961c15fe5a8ec2bb43f28c6d4db3ccde47c99
Sergio Prieto García <serprigar@gmail.com>
2024-03-19 14:23:04 +0100

    feat(RegularRound.java): add RegularRound.java

---
+++ src/main/java/edu/badpals/brunosbox/Rounds/RegularRound.java
@@ -0,0 +1,37 @@
+package edu.badpals.brunosbox.Rounds;
+
+public class RegularRound implements Round{
+    private String roundScore = "";
+    private byte redBoxerScore = 0;
+    private byte blueBoxerScore = 0;
+
+    public RegularRound(String roundScore) {
+        this.roundScore = roundScore;
+    }
+
+    public void setRedBoxerScore(int redBoxerScore) {
+        this.redBoxerScore = (byte)redBoxerScore;
+    }
+
+    public void setBlueBoxerScore(int blueBoxerScore) {
+        this.blueBoxerScore = (byte)blueBoxerScore;
+    }
+
+    public void boxerRoundScore(){
+        if (this.roundScore == "10 - 9"){
+            setRedBoxerScore(10);
+            setBlueBoxerScore(9);
+        }else{
+            setRedBoxerScore(9);
+            setBlueBoxerScore(10);
+        }
+    }
+
+    public byte getRedBoxerScore() {
+        return redBoxerScore;
+    }
+
+    public byte getBlueBoxerScore() {
+        return blueBoxerScore;
+    }
+}
